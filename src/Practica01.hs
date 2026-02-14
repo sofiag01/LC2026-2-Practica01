e4b45b3bab5e940d260bad534a59aeea6903070b
Emanuel-Duran <emanuelduran@ciencias.unam.mx>
2026-02-14 10:51:03 -0600

    Primeros ejercicios

--- src/Practica01.hs
+++ src/Practica01.hs
@@ -12,23 +12,34 @@ data Shape = Circle Float | --representa el radio
 
 --Funcion que calcula el area de las figuras
 area :: Shape -> Float
-area = undefined
+area (Circle rad) = pi * rad^2
+area (Square lado) = lado^2
+area (Rectangle b h) = b*h
+area (Triangle lad) = ((lad^2)*(sqrt 3))/4
+area (Trapeze bMay bMen h) = ((bMay + bMen)*h)/2
 
 --Funcion que calcula el perimetro de las figuras
 perimeter :: Shape -> Float
-perimeter = undefined
+perimeter (Circle rad) = 2*pi*rad
+perimeter (Square lado) = lado*lado
+perimeter (Rectangle b h) = 2*b + 2*h
+perimeter (Triangle lad) = 3*lad
+perimeter (Trapeze bMay bMen h) =
+--Funcion auxiliar para calcular un lado del trapecio
+    let ladoT = sqrt(h^2+(bMay - bMen)^2)
+    in bMay + bMen + 2*ladoT
 
 
 --Ejercicio 2 (Les toca arreglar el sinonimo)
-type Point = Shape
+type Point = (Float, Float)
 
 -- Funcion para calcular la distancia entre dos puntos
 distance :: Point -> Point -> Float
-distance = undefined 
+distance (x1, y1) (x2, y2) = sqrt (((x2 - x1)^2) + ((y2 - y1)^2))
 
 --Funcion para calcular la distancia de un punto al origen
 from0 :: Point -> Float
-from0 = undefined
+from0 (x, y) = distance (0,0) (x, y)
 
 --Ejercicio 3
 data Haskellium = Undefined
@@ -62,8 +73,13 @@ conjuntoPotencia = undefined
 
 --Implementacion
 
-data OneTwoTree a = Undefinedd
+data OneTwoTree a = Void
+                  | Node a (OneTwoTree a)
+                  | Branch a (OneTwoTree a) (OneTwoTree a)
+                   deriving (Show)
 
 --Ejercicio 2
 suma :: OneTwoTree Int -> Int
-suma = undefined
+suma Void = 0
+suma (Node a b) = a + suma b
+suma (Branch a b1 b2) = a + suma b1 + suma b2
